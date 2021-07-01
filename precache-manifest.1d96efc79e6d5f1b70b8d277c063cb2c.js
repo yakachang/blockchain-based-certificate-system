88cc7e228c2c8828a315bcaf1009b6a9431431e9
yakachang <yichen@nlplab.cc>
2021-07-01 17:50:17 +0800

    Updates

---
+++ precache-manifest.1d96efc79e6d5f1b70b8d277c063cb2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fe19d548ca36016658f",
+    "url": "/blockchain-based-certificate-system/static/css/main.9c4d4f76.chunk.css"
+  },
+  {
+    "revision": "6fe19d548ca36016658f",
+    "url": "/blockchain-based-certificate-system/static/js/main.92a5495c.chunk.js"
+  },
+  {
+    "revision": "486528c33c24601316cb",
+    "url": "/blockchain-based-certificate-system/static/js/runtime~main.7aefcb35.js"
+  },
+  {
+    "revision": "5d62b0a5d1cfb50084a9",
+    "url": "/blockchain-based-certificate-system/static/css/2.b49b57c6.chunk.css"
+  },
+  {
+    "revision": "5d62b0a5d1cfb50084a9",
+    "url": "/blockchain-based-certificate-system/static/js/2.a815a1b2.chunk.js"
+  },
+  {
+    "revision": "6eb78133d90b64ab8efd4251cdda7307",
+    "url": "/blockchain-based-certificate-system/static/media/bg.6eb78133.jpeg"
+  },
+  {
+    "revision": "c7f30458f217b2a5750b88f599fb1e24",
+    "url": "/blockchain-based-certificate-system/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/blockchain-based-certificate-system/precache-manifest.1d96efc79e6d5f1b70b8d277c063cb2c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/blockchain-based-certificate-system/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
